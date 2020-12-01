0e8df1b2f2070ef6084ddc86ea1fdf313743273b
ke1802rer8 <45431245+ke1802rer8@users.noreply.github.com>
2020-12-01 19:17:39 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $(".btn").click(function(){
+        $(".input").toggleClass("active").focus;
+        $(this).toggleClass("animate");
+        $(".input").val("");
+      });
+      
+
+    
+});
